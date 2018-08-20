59a46424547328572bc6292a19f72c590f8fdfe1
Rohit Premkumar <kumar.rohitp098@gmail.com>
2018-08-20 17:19:43 -0400

    added vue.js file and adjusted flexbox

---
+++ vue.js
@@ -0,0 +1,12 @@
+var post1 = new Vue ({
+  el: '#post1',
+  data: {
+    user: 'Kim Jong Un',
+    action: ' updated his profile picture',
+    img_src: 'https://e3.365dm.com/18/04/992x558/skynews-kim-kim-jong-un_4288585.jpg?20180421115011',
+    comment1: {
+      user: 'Dennis Rodman',
+      content: 'Cool picture man!'
+    }
+  }
+})
